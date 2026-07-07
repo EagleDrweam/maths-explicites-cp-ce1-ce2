5c3fbbbc9c8b240b999309c844481669c2d0d985
Gwendolyn <34269295+EagleDrweam@users.noreply.github.com>
2026-07-07 20:32:12 +0200

    Delete maths-explicites-cp-ce1-ce2 directory

--- maths-explicites-cp-ce1-ce2/service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='maths-explicites-v1';
-const FILES=['./','./index.html','./manifest.json','./assets/css/style.css','./assets/js/app.js','./data/seances/periode1/seance01.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
